3506f9c95d551829cd5f4c8ba1618cc58714396d
Mark Calcagno <mcalcagn@gmail.com>
2023-02-26 13:12:21 -0600

    images and links to sites now work on portfolio page. no styling yet

--- src/components/pages/Portfolio.js
+++ src/components/pages/Portfolio.js
@@ -1,4 +1,8 @@
 import React from 'react';
+import project2Image from '../../assets/images/SoBadItsGood.png'
+import project1Image from '../../assets/images/CardsAgainstApathyScreenshot.png'
+import homework1Image from '../../assets/images/techblog.png'
+import homework2Image from '../../assets/images/notetaker.png'
 
 export default function Portfolio() {
   return (
@@ -8,20 +12,17 @@ export default function Portfolio() {
             <h4 id="my-work">My Work</h4>
             <figure className="figure">
                 <a href="https://so-bad-its-good.herokuapp.com/">
-                    <img src="./assets/images/SoBadItsGood.png" alt="A project I made with 3 other bootcamp students" className="work-images-recent"/>
+                    <img src={project2Image} alt="A project I made with 2 other bootcamp students" className="portfolioImage"/>
                 </a>
-                <div>
-                    <img src="./assets/images/screenshot.png" alt="a website I repaired from my first homework assignment advertising Search Engine Optimization" className="work-images"/>
-                </div>
-                <div>
-                    <img src="./assets/images/CardsAgainstApathyScreenshot.png" alt="A project I made with 3 other bootcamp students" className="work-images"/>
-                </div>
-                <div>
-                    <img src="./assets/images/FutureProjects3.png" alt="a placeholder until future projects are completed" className="work-images"/>
-                </div>
-                <div>
-                    <img src="./assets/images/FutureProjects4.png" alt="a placeholder until future projects are completed" className="work-images"/>
-                </div>
+                <a href="https://the-fighting-mongooses-official.github.io/Cards-Against-Apathy/">
+                    <img src={project1Image} alt="A project I made with 3 other bootcamp students" className="portfolioImage"/>
+                </a>
+                <a href="https://calcagno-tech-blog.herokuapp.com/">
+                    <img src={homework1Image} alt="tech-blog" className="portfolioImage"/>
+                </a>
+                <a href="https://note-taker-calcagno.herokuapp.com/">
+                    <img src={homework2Image} alt="note-taker" className="portfolioImage"/>
+                </a>
             </figure>
         </section>
     </div>
