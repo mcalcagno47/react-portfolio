0f61a79a9a5bb8a556c9627f9c71e0a552580816
Mark Calcagno <mcalcagn@gmail.com>
2023-02-26 22:13:43 -0600

    added logos to footer and changed font

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,13 +1,22 @@
 import React from 'react';
+import github from '../assets/images/github.png'
+import gmail from '../assets/images/gmail.png'
+import linkedin from '../assets/images/linkedin.png'
 
 function Footer() {
     return (
         <footer className="footer">
             <div className="footerContent">
                 <p>                
-                    <a href="https://github.com/mcalcagno47">GitHub</a>
-                    <a href="https://www.linkedin.com/in/mark-calcagno-b96214a2/">LinkedIn</a>
-                    <a href="https://mail.google.com/mail/?view=cm&source=mailto&to=mcalcagn@gmail.com">Gmail</a>
+                    <a href="https://github.com/mcalcagno47">
+                        <img src={github} alt="github"></img>          
+                    </a>                    
+                    <a href="https://www.linkedin.com/in/mark-calcagno-b96214a2/">
+                        <img src={linkedin} alt="linkedin"></img>          
+                    </a>
+                    <a href="https://mail.google.com/mail/?view=cm&source=mailto&to=mcalcagn@gmail.com">
+                        <img src={gmail} alt="gmail"></img>          
+                    </a>
                 </p>
             </div>
         </footer>
